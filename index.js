0f8c0fface4ae34e25b50f501435d3994adfd2ec
MadhavT-1 <62096719+MadhavT-1@users.noreply.github.com>
2022-10-02 14:47:40 +0530

    Password Generator

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const otp = require('./OTP Generator/otpgenerate')
+const password = require('./PasswordGenerator/password')
 // import { generate } from './OTP Generator'
 
 console.log('====================================');
 console.log(otp());
+console.log('====================================');
+
+console.log('====================================');
+console.log("Generated Password = " + password());
 console.log('====================================');
