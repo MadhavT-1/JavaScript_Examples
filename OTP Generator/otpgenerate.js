8ecbbb6ffa6ba7c4a2843e930f8a995dd980c628
MadhavT-1 <62096719+MadhavT-1@users.noreply.github.com>
2022-10-02 14:42:35 +0530

    Otp Generator

---
+++ OTP Generator/otpgenerate.js
@@ -0,0 +1,13 @@
+const generate = () => {
+    let otp = ''
+    let numbers = '0123456789'
+    for (let i = 0; i < 6; i++) {
+        otp += numbers[Math.floor(Math.random() * 10)];
+    }
+    return otp;
+
+}
+
+
+module.exports = generate;
+// export default generate;
---
+++ index.js
@@ -0,0 +1,6 @@
+const otp = require('./OTP Generator/otpgenerate')
+// import { generate } from './OTP Generator'
+
+console.log('====================================');
+console.log(otp());
+console.log('====================================');
